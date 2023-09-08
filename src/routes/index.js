9e224c34320ba8e40f4c6b320388517ea7ac2bd7
Faruq-Hameed <faruqhameed1@gmail.com>
2023-09-08 13:54:56 +0100

    added logging logic

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,9 @@ const { register } = require('../controllers/authControllers');
 
 // Registration route
 router.post('/register', register);
-
+router.get('/register', (req, res)=>{
+    res.status(200).json({message: 'please register via POST request'});
+})
 router.post('/', (req, res)=>{
     console.log(req.body);
     res.status(200).json({success: 'success'});
