a8bd3b757721cc2fb4e77691deb1687cbb86950d
Faruq-Hameed <faruqhameed1@gmail.com>
2023-09-08 16:18:44 +0100

    added get users route

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,8 @@ const {User} = require('../db');
 router.post('/register', register);
 router.get('/register', async (req, res)=>{
     const users = await User.find({})
+    .sort({ createdAt: -1 })
+
     res.status(200).json({message: 'succesful', users});
 })
 router.post('/', (req, res)=>{
